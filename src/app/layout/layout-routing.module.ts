7d2e8959e772debd94d0ad50d9db3d837afc46f0
cepragamihail <yahim09@gmail.com>
2019-11-30 12:25:27 +0200

    initial app arhitecture

---
+++ src/app/layout/layout-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+
+const routes: Routes = [
+  {
+  path: '',
+  component: LayoutComponent,
+  children: [
+      {
+        path: '',
+        redirectTo: 'home'
+      },
+      {
+        path: 'home',
+        loadChildren: './home/home.module#HomeModule'
+      }
+  ]
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LayoutRoutingModule { }
